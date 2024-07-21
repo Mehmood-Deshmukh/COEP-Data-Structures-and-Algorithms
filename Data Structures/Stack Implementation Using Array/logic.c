fba0a82b93294b71d2cbb978eef155c3d649737a
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-07-21 18:20:34 +0530

    Stack Implementation using Array

---
+++ Data Structures/Stack Implementation Using Array/logic.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include "./header.h"
+
+void init(Stack *s, int size){
+    if(size < 1){
+        return;
+    }
+    s->size = size;
+    s->top = 0;
+    s->array = (int *) malloc(sizeof(int) * size);
+
+    return;
+}
+
+int isEmpty(Stack s){
+    if((s.top) == 0){
+        return 1;
+    }
+
+    return 0;
+}
+
+int isFull(Stack s){
+    if((s.top) == (s.size)){
+        return 1;
+    }
+
+    return 0;
+}
+
+void push(Stack *s, int data){
+    if (isFull(*s)){
+        return;
+    }
+    s->array[s->top++] = data;
+    return;
+}
+
+int peek(Stack s){
+    if (isEmpty(s)){
+        return INT_MIN;
+    }
+
+    return s.array[s.top - 1];    
+}
+int pop(Stack *s){
+    if (isEmpty(*s)){
+        return INT_MIN;
+    }
+
+    return s->array[--(s->top)];
+}
+void display(Stack s){
+    if(s.top == 0){
+        printf("The Stack is Empty!\n");
+        return;
+    }
+
+    printf("Displaying the stack\n");
+
+    for(int i = 0; i < s.top; i++){
+        printf("%d ", s.array[i]);
+    }
+    printf("\n");
+}
---
+++ Data Structures/Stack Implementation Using Array/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "logic.c"
+
+int main() {
+    Stack s;
+    init(&s, 3);
+    display(s);
+    printf("Is the Stack Empty? %d\n", isEmpty(s));
+
+    push(&s, 1);
+    push(&s, 2);
+    push(&s, 3);
+
+    display(s);
+    printf("Is the Stack Full? %d\n", isFull(s));
+
+    pop(&s);
+    pop(&s);
+
+    display(s);
+
+    printf("Peeked Element: %d\n", peek(s));
+
+    pop(&s);
+
+    printf("Is the Stack Empty? %d\n", isEmpty(s));
+    return 0;
+}
