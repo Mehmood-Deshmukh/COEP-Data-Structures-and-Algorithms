d6489334273437f71cf183c2ac2f5742d60eed7a
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-07-28 15:43:02 +0530

    Queue Implementation using Singly LinkedList

---
+++ Data Structures/Queue Implementation using Singly LinkedList/header.h
@@ -0,0 +1,16 @@
+typedef struct Node{
+    int data;
+    struct Node *next;
+} Node;
+
+typedef struct Queue{
+    Node *head;
+    Node *tail;
+} Queue;
+
+void init(Queue *q);
+void enQueue(Queue *q, int data);
+int deQueue(Queue *q);
+int isEmpty(Queue q);
+int peek(Queue q);
+void display(Queue q);
