40c35e050deba96429c00ce7fceb346ec8aed159
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-07-28 15:24:50 +0530

    Stack Implementation Using Singly LinkedList

---
+++ Data Structures/Stack Implementation Using Singly LinkedList/header.h
@@ -0,0 +1,13 @@
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+typedef Node *Stack;
+
+void init(Stack *s);
+void push(Stack *s, int data);
+int pop(Stack *s);
+int peek(Stack s);
+int isEmpty(Stack s);
+void display(Stack s);
---
+++ Data Structures/Stack Implementation Using Singly LinkedList/logic.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "./header.h"
+
+void init(Stack *s){
+    *s = NULL; 
+    return;  
+}
+
+void push(Stack *s, int data){
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if(!newNode) return;
+
+    newNode->data = data;
+    newNode->next = *s;
+
+    *s = newNode;
+    return;
+}
+
+int pop(Stack *s){
+    if(isEmpty(*s)) return INT_MIN;
+
+    Node *removedNode;
+    int removedElement;
+
+    removedNode = *s;
+    removedElement = removedNode->data;
+    *s = (*s)->next;
+
+    free(removedNode);
+    return removedElement;
+}
+
+int peek(Stack s){
+    if(isEmpty(s)) return INT_MIN;
+
+    return s->data;
+}
+
+int isEmpty(Stack s){
+    if(!s) return 1;
+
+    return 0;
+}
+
+void display(Stack s){
+    if(isEmpty(s)){
+        printf("Stack is Empty!\n");
+        return;
+    }
+
+    printf("Displaying stack: ");
+    Node *temp = s;
+    while(temp){
+        printf("%d | ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\b\b \n");
+    return;
+}
