824bf3b76f87be178278819b7ac0438d83b71326
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-10-30 18:05:24 +0530

    add: assignment 6

---
+++ Lecture Assignments/Assignment 6/header.h
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<errno.h>
+
+#define MAX_SIZE 200
+
+typedef struct max_heap{
+    int *array;
+    int rear;
+    int size;
+} max_heap;
+
+typedef struct min_heap{
+    int *array;
+    int rear;
+    int size;
+} min_heap;
+
+void init_max_heap(max_heap *h);
+void init_min_heap(min_heap *h);
+
+int is_empty_max_heap(max_heap h);
+int is_empty_min_heap(min_heap h);
+
+void handle_is_full_max_heap(max_heap *h);
+void handle_is_full_min_heap(min_heap *h);
+
+void swap(int *a, int *b);
+int get_parent_index(int index);
+
+void insert_max_heap(max_heap *h, int data);
+void insert_min_heap(min_heap *h, int data);
+
+void heapify_max_heap(max_heap *h);
+void heapify_min_heap(min_heap *h);
+
+int remove_max_heap(max_heap *h);
+int remove_min_heap(min_heap *h);
+
+void print_max_heap(max_heap h);
+void print_min_heap(min_heap h);
+
+void heap_sort_max_heap(int *array, int size);
+void heap_sort_min_heap(int *array, int size);
---
+++ Lecture Assignments/Assignment 6/main.c
@@ -0,0 +1,66 @@
+#include "header.h"
+
+void print_array(int *array, int size);
+
+int main(int argv, char *argc[]){
+    if(argv < 2){
+        printf("Usage: %s <file with numbers to sort>\n", argc[0]);
+        exit(1);
+    }
+
+    FILE *file = fopen(argc[1], "r");
+    if(file == NULL){
+        perror("Error opening file: ");  
+        exit(1);
+    }
+
+    int size;
+    fscanf(file, "%d", &size);
+
+    int *array = (int *) malloc(sizeof(int) * size);
+    for(int i = 0; i < size; i++){
+        fscanf(file, "%d", &array[i]);
+    }
+
+    fclose(file);
+
+    int *array_copy = (int *) malloc(sizeof(int) * size);
+    for(int i = 0; i < size; i++){
+        array_copy[i] = array[i];
+    }
+
+    printf("Sorting using max heap\n");
+
+    printf("Before sorting: ");
+    print_array(array, size);
+
+    heap_sort_max_heap(array, size);
+
+    printf("After sorting: ");
+    print_array(array, size);
+
+    printf("\n");
+
+    printf("Sorting using min heap\n");
+
+    printf("Before sorting: ");
+    print_array(array_copy, size);
+
+    heap_sort_min_heap(array_copy, size);
+
+    printf("After sorting: ");
+    print_array(array_copy, size);
+
+    free(array);
+    free(array_copy);
+
+    return 0;
+}
+
+void print_array(int *array, int size){
+    for(int i = 0; i < size; i++){
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+}
